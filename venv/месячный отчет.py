06689c2ade30e5c998471ae40af1e8d209d7960c
Seppuku <Iamemperor@yandex.ru>
2023-05-29 17:47:14 +0300

    Реализация кода через pandas(без использования json)

--- venv/месячный отчет.py
+++ venv/месячный отчет.py
@@ -1,56 +1,51 @@
 import os
-import pandas as pd
 import json
+import pandas as pd
 import re
 import itertools
-files = os.listdir("C:\\Users\Iamem\Desktop\Апрель")
-os.chdir("C:\\Users\Iamem\Desktop\Апрель")
-json_files = [i for i in files if i.endswith('.json')]
-filesList = []
-names = [] # список имен сотрудников
+files = os.listdir("C:\\Users\Iamem\Desktop\Март")
 
-for file in json_files:  # создаем список словарей из json файлов ключ - строка, значения - с 1 по 36 колонку в таблицах
-    with open(file, encoding='utf-8') as f:
-        a = json.load(f)
-        filesList.append(a)
+os.chdir("C:\\Users\Iamem\Desktop\Март")
 
-job_times = []
+excel_files = [i for i in files if i.endswith('.xls') or i.endswith('.xlsx')]
+
+workers_names = []
+
+contact_name_pattern = re.compile(r'\d\.\d\.\d{0,1}\s{0}')
+
 contract_names = []
 
-for i in filesList:
-    for j in i[50:]: # отсекаем все до 50 строки
-        try:
-            if j['Column36'] > 0: # если отработанное время не 0, то добавляем имя сот-ка,
-                                 # рабочие часы по контракту и имя контракта
-                names.append(i[1]['Column3'])
-                job_times.append(round(j['Column36'], 1))
-                contract_names.append(j['Отчет по загрузке специалиста'])
-        except (KeyError, TypeError):
-            pass
-set_names = sorted(set(names))
+job_times = []
 
-contact_name_pattern = re.compile(r'\d\.\d\.\d{0,1}\s{0}') # регялрное выражение для номера контракта
+for file in excel_files:
+     data = pd.read_excel(file)
+     workers_names.append(data['Unnamed: 2'].loc[data.index[2]])
+     for i, j in data.tail(22).iterrows():
+         try:
+             if j['Unnamed: 35'] > 0:  # если отработанное время не 0, то добавляем имя сот-ка,
+                  # рабочие часы по контракту и имя контракта
+                 job_times.append(round(j['Unnamed: 35'], 1))
+                 contract_names.append(j['Unnamed: 1'])
+         except (KeyError, TypeError, ValueError):
+             pass
 
-num_cts = [contact_name_pattern.findall(c_name) for c_name in contract_names] # список спиков номеров контрактов
+num_cts = [contact_name_pattern.findall(c_name) for c_name in contract_names]
 
 num_contracts = list(itertools.chain(*num_cts)) # одномерный список контрактов
 
 set_contracts = list(sorted(set(num_contracts))) # множество номеров контрактов
 
-df = pd.DataFrame(columns=set_names, index=set_contracts)
+df = pd.DataFrame(columns=workers_names, index=set_contracts)
 
-for i in filesList:
-    for j in i[50:]:
+for i in excel_files:
+    data = pd.read_excel(i)
+    for k, j in data.tail(22).iterrows():
         try:
-            if j['Column36'] > 0:
-                df.at[contact_name_pattern.search(j['Отчет по загрузке специалиста']).group(),
-                i[1]['Column3']] = round(j['Column36'], 1)
-        except (KeyError, TypeError):
+            if j['Unnamed: 35'] > 0:
+                df.at[contact_name_pattern.search(j['Unnamed: 1']).group(), data['Unnamed: 2'].loc[data.index[2]]] = round(j['Unnamed: 35'], 1)
+        except (KeyError, TypeError, ValueError):
             pass
-
-
 print(df)
-print(set_contracts)
-# print(set_contracts)
-df.to_excel("C:\\Users\Iamem\Desktop\отчеты_апрель.xlsx")
+
+df.to_excel("C:\\Users\Iamem\Desktop\отчеты_март.xlsx")
 
